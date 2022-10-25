4a256cbc92e5de71f4a6ee1df41424c6505b6291
LiaTauber9 <100442290+LiaTauber9@users.noreply.github.com>
2022-10-25 11:16:07 +0300

    <d ch>

--- Week3/Week3Day1/W3D1_daily_challenge/w3d1_daily_challenge.js
+++ Week3/Week3Day1/W3D1_daily_challenge/w3d1_daily_challenge.js
@@ -1,7 +1,54 @@
-let planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+// let planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+// for(let p of planets){
+//     const planetDiv = document.createElement('div');
+//     planetDiv.classList.add('planet', p);
+//     planetDiv.textContent = p;
+//     document.getElementsByClassName('listPlanets')[0].appendChild(planetDiv);
+// }
+
+// option 2 + bonus
+let planets = [
+    {
+        name: 'Mercury',
+        moon: 2
+    },
+    {
+        name: 'Venus',
+        moon: 1
+    },
+    {
+        name: 'Earth',
+        moon: 1
+    },
+    {
+        name: 'Mars',
+        moon: 5
+    },
+    {
+        name: 'Jupiter',
+        moon: 0
+    },
+    {
+        name: 'Saturn',
+        moon: 1
+    },
+    {
+        name: 'Uranus',
+        moon: 3
+    },
+    {
+        name: 'Neptune',
+        moon: 1
+    }
+]
 for(let p of planets){
-    const planetDiv = document.createElement('div');
-    planetDiv.classList.add('planet', p);
-    planetDiv.textContent = p;
-    document.getElementsByClassName('listPlanets')[0].appendChild(planetDiv);
-}
+        const planetDiv = document.createElement('div');
+        planetDiv.classList.add('planet', p.name);
+        planetDiv.textContent = p.name;
+        document.getElementsByClassName('listPlanets')[0].appendChild(planetDiv);
+        for(let moon = 1; moon <= p.moon; moon++){
+            const moonDiv = document.createElement('div');
+            moonDiv.classList.add('moon');
+            document.getElementsByClassName(p.name)[0].appendChild(moonDiv)
+        }
+    }
