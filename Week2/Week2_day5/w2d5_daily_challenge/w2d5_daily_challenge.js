f8fc013d62d4c5a7e6d7fb647562934d0c28551f
LiaTauber9 <100442290+LiaTauber9@users.noreply.github.com>
2022-10-25 09:52:13 +0300

    <week 2 day 5 daily challenge>

--- Week2/Week2_day5/w2d5_daily_challenge/w2d5_daily_challenge.js
+++ Week2/Week2_day5/w2d5_daily_challenge/w2d5_daily_challenge.js
@@ -1,12 +1,13 @@
 function decrease(){
-    let butllesArr = [99];
+    let buttllesArr = [99];
     let buttlesSum = 99;
     for(let i = 1; buttlesSum > i; i++){
-        butllesArr.push(butlles[i-1]-i)
+        buttllesArr.push(buttllesArr[i-1]-i)
         buttlesSum -= i; 
-        console.log(`${buttlles[i-1]} bottles of beer on the wall \n
-        ${butlles[i-1]} bottles of beer \n
+        console.log(`${buttllesArr[i-1]} bottles of beer on the wall \n
+        ${buttllesArr[i-1]} bottles of beer \n
         Take ${i} down, pass it around \n
-        ${butlles[i]} bottles of beer on the wall`)
+        ${buttllesArr[i]} bottles of beer on the wall`)
     }
 }
+decrease();
