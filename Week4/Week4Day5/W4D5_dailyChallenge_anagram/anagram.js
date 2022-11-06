d260ae388ca5917f4718c4a3fef1a727000633f5
LiaTauber9 <100442290+LiaTauber9@users.noreply.github.com>
2022-11-06 09:46:40 +0200

    <>

--- Week4/Week4Day5/W4D5_dailyChallenge_anagram/anagram.js
+++ Week4/Week4Day5/W4D5_dailyChallenge_anagram/anagram.js
@@ -2,7 +2,7 @@ let str1 = 'Astronomer'.toLowerCase();
 let str2 = 'Moon starer';
 
 function isAnigram(str1, str2){
-    const st1 = str1.toLowerCase().split("").filter((e)=>e!=" ");
-    const st2 = str2.toLowerCase().split("").filter((e)=>e!=" ");
-    return st1.every(e=>st2.includes(e)) && st1.length == st2.length
+    const st1 = str1.toLowerCase().split("").filter((e)=>e!=" ").sort().join();
+    const st2 = str2.toLowerCase().split("").filter((e)=>e!=" ").sort().join();
+    return st1 === st2;
 }console.log(isAnigram(str1,str2))
