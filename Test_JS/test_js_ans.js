a1d18154b25b05ad2801c1d0ebc8cfd72c965836
LiaTauber9 <100442290+LiaTauber9@users.noreply.github.com>
2022-11-17 19:59:23 +0200

    <test>

--- Test_JS/test_js_ans.js
+++ Test_JS/test_js_ans.js
@@ -132,7 +132,7 @@ const initials = input1.split(" ").map(elem => elem[0]).join("");
 console.log(initials)//'GRRM'
 //Object and Classes
 //20
-function changeTshirt (myshirt){
+function changeTshirt(myshirt) {
     console.log("myshirt", myshirt);
     myshirt.color = "red" //change the object tshirt in the function and allso in the global variable
     console.log("myshirt", myshirt); //this is the parameter
@@ -141,19 +141,19 @@ function changeTshirt (myshirt){
 }
 
 const tshirt = {
-    color : "blue",
-    price : 10
+    color: "blue",
+    price: 10
 }
 
 changeTshirt(tshirt)
 //21
-function changeTshirt (myshirt){
+function changeTshirt(myshirt) {
     let myshirt1 = {};
     let myShirtArr = Object.keys(myshirt)
-    for(key of myShirtArr){
-        myshirt1[key] = myshirt[key] 
+    for (key of myShirtArr) {
+        myshirt1[key] = myshirt[key]
     }
-    
+
     console.log("myshirt", myshirt1);
     myshirt1.color = "red"
     console.log("myshirt1", myshirt1); //this is the parameter
@@ -163,64 +163,64 @@ function changeTshirt (myshirt){
 const football = {
     type: "sport",
     clubs: {
-      france : "Paris Saint-Germain",
-      spain : "Atlético Madrid",
+        france: "Paris Saint-Germain",
+        spain: "Atlético Madrid",
     }
-  }  
- const {france: franceTeam, spain: spainTeam} = football.clubs
- //23 
- function retrieveSports (franceTeam,spainTeam) {
+}
+const { france: franceTeam, spain: spainTeam } = football.clubs
+//23 
+function retrieveSports(franceTeam, spainTeam) {
     const sentence = `My favorite football teams are ${franceTeam} and ${spainTeam}`;
     console.log(sentence);
- }
- //24
- class Item {
-    constructor(nameProduct, priceProduct) { 
-          this.name = nameProduct;
-          this.price = priceProduct;
+}
+//24
+class Item {
+    constructor(nameProduct, priceProduct) {
+        this.name = nameProduct;
+        this.price = priceProduct;
     }
- 
-    displayInfo () {
-         console.log(this.name + " is for $ " + this.price)
-    }
- } 
- 
- const cake = new Item("Chocolate Cake", 10);
- cake.displayInfo();// "Chocolate Cake is for $ 10"
 
- //Promises
- //25
- Promise.resolve('Success!')
-  .then(data => {
-    return data.toUpperCase()
-  })
-  .then(data => {
-    console.log(data)
-  })
+    displayInfo() {
+        console.log(this.name + " is for $ " + this.price)
+    }
+}
+
+const cake = new Item("Chocolate Cake", 10);
+cake.displayInfo();// "Chocolate Cake is for $ 10"
+
+//Promises
+//25
+Promise.resolve('Success!')
+    .then(data => {
+        return data.toUpperCase()
+    })
+    .then(data => {
+        console.log(data)
+    })
 // output is: console.log('SUCCESS!') becous the first 'then' get the result 'Success!', convert the result to uppercase and return a new promis with the result 'SUCCESS!', the second 'then' get this result and print it to the console and return a new promis with no value at the result
 //26
-const p = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-       resolve("OK");
+const p = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve("OK");
     }, 2000);
- });
- 
- p.then().then(function(data) {
+});
+
+p.then().then(function (data) {
     console.log(data);
- }); //after 2 sec "OK" printed to the console
- //27
- async function test() {
+}); //after 2 sec "OK" printed to the console
+//27
+async function test() {
     let result = 'first!';
     const promise = new Promise((resolve, reject) => {
-      setTimeout(() => resolve('done!'), 1000);
+        setTimeout(() => resolve('done!'), 1000);
     });
-    
+
     result = await promise;
-  
+
     console.log(result);
-  }
-  
-  test();// 'done!' becouse of the 'await' - the function is waiting for the result of the promise, the result ('done!) asaigned as the value of result and printed to the console
+}
+
+test();// 'done!' becouse of the 'await' - the function is waiting for the result of the promise, the result ('done!) asaigned as the value of result and printed to the console
 //28
 // Use async await, and fetch a fact on cats and display it. Use this third party API : https://catfact.ninja/fact
 
@@ -228,14 +228,14 @@ const p = new Promise(function(resolve, reject) {
 // In the JS file, create 2 functions : one to fetch data from API, the other one to display it on the page using the DOM
 // Make sure to use try and catch
 
-function getFact(){
-        const promis = async function(){
-            return await fetch('https://catfact.ninja/fact').then(res => res.json())
-        } 
-        promis().then(res => printFact(res.fact)).catch(() => printFact("oooops, couldn't find result"))
+function getFact() {
+    const promis = async function () {
+        return await fetch('https://catfact.ninja/fact').then(res => res.json())
+    }
+    promis().then(res => printFact(res.fact)).catch(() => printFact("oooops, couldn't find result"))
 }
 
-function printFact(fact){
+function printFact(fact) {
     const factDiv = document.createElement('div');
     factDiv.textContent = fact;
     document.body.appendChild(factDiv);
@@ -245,3 +245,41 @@ factBtn.textContent = 'Fact about Cat';
 factBtn.addEventListener('click', getFact);
 document.body.appendChild(factBtn);
 
+// BONUS
+class Book{
+    constructor(title, author, date, pages, timesRead){
+        this.title = title,
+        this.author = author,
+        this.creationDate = date,
+        this.pagesNumber = pages,
+        this.timesRead = timesRead
+    }
+    read(times) {
+        return this.timesRead += times*1
+    }    
+}
+
+class Manual extends Book{
+    constructor(title, author, date, pages, timesRead){
+        super(title, author, date, pages, timesRead)
+    }
+    isThrown(){
+        const currentYear = new Date().getFullYear(); 
+        this.thrown = currentYear - this.creationDate > 5 ? true : false
+        return this.timesRead ? 'yes' : 'no'
+    }    
+}
+class Novel extends Book{
+    constructor(title, author, date, pages, timesRead){
+        super(title, author, date, pages, timesRead)
+    }
+    isThrown(){
+        this.thrown = this.timesRead > 100 ? true : false
+        return this.timesRead ? 'yes' : 'no'
+    }    
+}
+const Book1 = new Novel('Top Secret Shuttle Building Manual', 'Redacted', '2013-01-01', 1147, 1);
+const Book2 = new Manual('Pride and Prejudice', 'Jane Austen', '1813-01-01', 432, 32);
+console.log(`Is the book ${Book1.title} should be thrown? ${Book1.isThrown()}`)
+console.log(`Is the book ${Book2.title} should be thrown? ${Book2.isThrown()}`)
+console.log(Book1.read(5));
