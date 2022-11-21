780bda33165dd09e8c377b32b21da1fa3f672e2c
LiaTauber9 <100442290+LiaTauber9@users.noreply.github.com>
2022-11-21 09:52:18 +0200

    daily

---
+++ Week7/Day_1/Daily/script.js
@@ -0,0 +1,13 @@
+//part 1
+const largNum = require('./main.js');
+const b = 5;
+console.log(largNum.largeNumber+b);
+
+//part 3
+const currentdate = new Date(); 
+const datetime = `The date and time are currently: ${currentdate.getDay()} ${currentdate.getMonth()} ${currentdate.getDate} ${currentdate.getFullYear()}`
+// + " @ "  
+//                 + currentdate.getHours() + ":"  
+//                 + currentdate.getMinutes() + ":" 
+//                 + currentdate.getSeconds();
+console.log(dateTime);
