722592027149852fa2cca947b87a704cc4ace4fd
LiaTauber9 <100442290+LiaTauber9@users.noreply.github.com>
2022-11-28 19:47:05 +0200

    w8d1 daily

--- js72/myapp/src/index.js
+++ js72/myapp/src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hello from './components/xpw8d1/Hello';
-import UserFavoriteColors from './components/xpw8d1/UserFavoriteColors';
-import Exercise4 from './components/xpw8d1/Exercise4';
-
+import Hello from './components/W8D1/XP/Hello';
+import UserFavoriteColors from './components/W8D1/XP/UserFavoriteColors';
+import Exercise4 from './components/W8D1/XP/Exercise4';
+import Carouselimg from './components/W8D1/Daily/Carousel';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <Hello /> */}
     {/* <UserFavoriteColors/> */}
-    <Exercise4 />
+    {/* <Exercise4 /> */}
+    <Carouselimg />
   </React.StrictMode>
 );
 
